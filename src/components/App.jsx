edc5aca14eb29b9a451f883014ac9036099f6143
IEIrodov <nikhilindia30111999@gmail.com>
2020-06-06 06:29:58 +0530

    Conditional Rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,12 @@ import Login from "./Login";
 
 var isLoggedIn = false;
 
-function renderConditionally() {
-  if (isLoggedIn === true) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
-
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    <div className="container">
+      {isLoggedIn === true ? <h1>Hello</h1> : <Login />}
+    </div>
+  );
 }
 
 export default App;
