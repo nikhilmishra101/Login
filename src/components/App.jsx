43d72d3cab46bc887ea5da628af3547f2e729bf3
IEIrodov <nikhilindia30111999@gmail.com>
2020-06-06 06:01:02 +0530

    making changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
+var isLoggedIn = true;
+
+function renderConditionally() {
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1>;
+  } else {
+    <form className="form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>;
+  }
+}
+
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  return <div className="container">{renderConditionally()}</div>;
 }
 
 export default App;
