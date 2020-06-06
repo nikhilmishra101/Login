a56392f3d2e6d023985a0e852ee56adb63cec310
IEIrodov <nikhilindia30111999@gmail.com>
2020-06-06 06:27:23 +0530

    Adding components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Login from "./Login";
 
 var isLoggedIn = false;
 
@@ -6,13 +7,7 @@ function renderConditionally() {
   if (isLoggedIn === true) {
     return <h1>Hello</h1>;
   } else {
-    return (
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    );
+    return <Login />;
   }
 }
 
